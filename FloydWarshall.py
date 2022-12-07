c2184f0e89862d7c909251dd597a70b36c8c2811
TimBrusten <78920727+TimBrusten@users.noreply.github.com>
2022-12-07 17:10:44 +0100

    ajout des commentaires

--- FloydWarshall.py
+++ FloydWarshall.py
@@ -1,16 +1,21 @@
 import numpy as np
 import csv
+# import des modules nécessaires
 
 result = np.array(
     list(csv.reader(open("LINFO1114_projet\matrix.csv", "r"), delimiter=";"))).astype("float")
 print(result)
+# ouverture et transformation du fichier.csv en matrice numpy
 
 
 def Floyd_Warshall(C: np.matrix):
+    # boucle principale permettant l'itération pour les noeuds intermédiaires
     for k in range(len(C[0])):
         for i in range(len(C[0])):
+            # boucle i et j pour chaque paire de coordonnées dans la matrice de distances
             for j in range(len(C[0])):
                 if (C[i][j] > C[i][k]+C[k][j]):
+                    # si le chemin de i a j et plus court par en passant par le noeud intermédiaire : remplacer la valeur du chemin le plus court
                     C[i][j] = C[i][k]+C[k][j]
     return C
 
