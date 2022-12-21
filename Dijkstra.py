07fb3274cac8cbdcf94c225d0bc2d5de26fb08d0
ArthiChaut <arthur.fraipont@student.uclouvain.be>
2022-12-21 23:12:58 +0100

    matric

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,11 +1,9 @@
 import numpy as np
 Inf = 1.e+12
 
-
 def dijkstraDist(C, depart):
     # On récupère le nombre de sommets du graphe
     N = np.size(C, 0)
-
     # Initialisation du tableau des plus courts chemins
     # Le booléen pour savoir si le sommet a déjà été sélectionné
     pcc = list()
@@ -16,7 +14,6 @@ def dijkstraDist(C, depart):
     pcc[depart][0] = 0
     # Le premier sommet sélectionné est le sommet depart
     pcc[depart][1] = True
-
     # On compte le nombre de sommets sélectionnés
     cpt = 0
     while cpt != N-1:
@@ -25,30 +22,24 @@ def dijkstraDist(C, depart):
         minimum = Inf
         # Étape de relâchement
         for k in range(N):
-
             # Si le sommet k n’a pas encore été sélectionné
             if pcc[k][1] == False:
                 dist_uv = C[sommet_u][k]
                 # Distance totale du chemin s -> ... -> u -> v
                 dist_totale = dist_u + dist_uv
-
                 # Mise à jour du tableau des plus courts chemins
                 if dist_totale < pcc[k][0]:
                     pcc[k][0] = dist_totale
-
             # Mise à jour de la solution minimale à cette étape
                 if pcc[k][0] < minimum:
                     minimum = pcc[k][0]
                     prochain_sommet_select = k
-
         # On a traité complétement un sommet
         cpt = cpt + 1
-
         # Le sommet à traiter est sélectionné et d[u] est mis à jour
         sommet_u = prochain_sommet_select
         pcc[sommet_u][1] = True
         dist_u = pcc[sommet_u][0]
-
     return (pcc)
 
 #fusion des lignes individuelles en matrice
